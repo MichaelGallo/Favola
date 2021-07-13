b9f6122857e1cb452201083f5b19ae3479fb3af1
Michael Gallo <michael@hyperganic.com>
2021-07-14 00:27:33 +0200

    Added a VS project

---
+++ Source/Favola.cpp
@@ -0,0 +1,40 @@
+#include <memory>
+
+//#include "Platform.h"
+#include <QApplication>
+#include <QTranslator>
+
+class CFavola : public QApplication
+{
+public:
+	CFavola(int& argc, char** argv) : QApplication(argc, argv) {}
+
+	const QString& strFileOpenArgument() const	{ return m_strFileOpenArgument; }
+
+protected:
+	bool event(QEvent* poEvent);
+
+private:
+	QString m_strFileOpenArgument;
+};
+
+
+int main(int argc, char *argv[])
+{
+
+	CFavola oApp(argc, argv);
+	
+#ifdef Q_OS_MAC
+	poApp->processEvents();
+	poApp->processEvents();
+	
+	if (!poApp->strFileOpenArgument().isEmpty())
+	{
+		//open the game
+	}
+#endif
+
+	CMainWindow oMainWindow;
+
+	return oApp.exec();
+}
